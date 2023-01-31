078db6515d2df9a1d79d70466aaea18247f1199b
Anne <anne.tsugunyan@gmail.com>
2023-02-01 00:42:44 +0400

    Updating the footer

--- src/components/footer-policy/index.tsx
+++ src/components/footer-policy/index.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
-import { DoubleLogo, Link } from "@app/components";
+import { Container } from "react-bootstrap";
+import { Link } from "@app/components";
 import * as styles from "./index.module.scss";
 
 export const FooterPolicy = () => {
   return (
-    <Container>
-      <div className="d-flex align-items-center justify-content-between py-4">
-        <DoubleLogo />
-        <div className={styles.footerLogo}>
-          <Button to="/legal/privacy-policy" as={Link} variant="text">
-            Privacy Policy
-          </Button>
+    <div className={styles.FooterPolicy}>
+      <Container>
+        <div className="d-flex flex-lg-row flex-md-row flex-column align-items-lg-center justify-content-end">
+          <Link to="/legal/terms">Terms & Conditions</Link>
+          <Link to="/legal/privacy-policy">Privacy Policy</Link>
+          <Link to="/legal/academy">The Academy</Link>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import { DoubleLogo, Link } from "@app/components";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "@app/components";
 import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./index.module.scss";
 import { useSiteMetadata } from "@app/hooks";
+import logo from "./logo.svg";
 
 export const Footer = () => {
   const { contact_email, contact_phone } = useSiteMetadata();
@@ -11,30 +12,60 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Container>
-        <Row className="mt-lg-6 mb-lg-7 mt-md-4 mb-md-4 align-items-center">
-          <h2 className="d-md-block d-lg-none mb-3 ">Think we should talk?</h2>
-          <Col
-            lg={6}
-            md={{ span: 12, order: "2" }}
-            sm={{ span: 12, order: "2" }}
-            xs={{ span: 12, order: "2" }}
-          >
+        <div className={styles.headerLogo}>
+          <img src={logo} alt="Double logo" />
+          <Link to="https://www.linkedin.com/company/double-agency/">
+            <div className={styles.linkedinIcon}></div>
+          </Link>
+        </div>
+        <div className="mt-lg-5 mt-4 d-flex flex-lg-row flex-column justify-content-lg-between">
+          <div lg={6} md={12}>
             <h4 className="mb-3">Email us</h4>
             <div className="mb-3">{contact_email}</div>
             <h4 className="mb-3">Call us</h4>
             <div className="mb-4">{contact_phone}</div>
-            <Button as={Link} to="/contact" variant="outline-secondary">
+            <Button
+              as={Link}
+              to="/contact"
+              variant="outline-light"
+              className={styles.roundedButton}
+            >
               Contact us
             </Button>
-          </Col>
-          <Col className="mb-3 mb-lg-0" lg={{ span: 6, order: "2" }} md={12}>
-            <StaticImage
-              src="./footer_img.jpeg"
-              alt="Contact us"
-              className={styles.mobileImage}
-            />
-          </Col>
-        </Row>
+            <div className="my-5 d-flex flex-direction-row">
+              <div className={styles.mapIcon}></div>
+              <div>
+                Netherlands - Amsterdam
+                <br />
+                Prins Hendrikkade 21e, 1012 TL
+                <br />
+                8:00-20:00 Mon-Fri
+              </div>
+            </div>
+            <div className="mb-5 d-flex flex-direction-row">
+              <div className={styles.mapIcon}></div>
+              <div>
+                United States of America
+                <br />
+                680 South Cache Street, WY 83002
+              </div>
+            </div>
+          </div>
+          <div className="mb-3 mb-lg-0" lg={6} md={12}>
+            <h5 className="mb-1">Partners</h5>
+            <div className={styles.partnersLogo}>
+              <div>
+                <StaticImage src="./netlify.png" alt="Netlify logo" />
+              </div>
+              <div>
+                <StaticImage src="./Google.png" alt="Google partner logo" />
+              </div>
+              <div>
+                <StaticImage src="./drip.png" alt="Drip logo" />
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </footer>
   );
