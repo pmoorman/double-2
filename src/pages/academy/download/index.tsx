4ef2a00c76e1f1134b14cd94f85e23126625c70e
RekaSzendi <reka@double-agency.com>
2023-03-27 14:04:13 +0800

    new academy LPs for MENA ads

--- src/pages/academy/download/index.tsx
+++ src/pages/academy/download/index.tsx
@@ -1,75 +1,75 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { SEO, OgilvyForm } from "@app/components";
+import { SEO, OgilvyInlineForm, OgilvyLearnForm } from "@app/components";
 
 import * as styles from "./index.module.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
-const formId = "4880114";
+const formId = "4980653";
 
-const OglviyConcept2 = () => {
+const OglviyConcept4 = () => {
   return (
     <>
-      <SEO title="38 laws of growth marketing" />
-      <div>
+      <SEO title="38 laws to avoid startup disaster" />
+      <div className={styles.pageHero}>
         <Container>
-          <div className={styles.heading}>
-            <h1>
-              38 laws to <div>avoid startup </div>
-              <div> disaster</div>
-            </h1>
-            <p className="mt-5">
-              As a growth agency, we have worked with some of the
-              fastest-growing tech companies like TikTok, Alpian, Glowforge and
-              more.
-            </p>
-            <p className="mb-5 mb-lg-0">
-              We collected the 38 most important lessons that we’ve learned
-              along the way.
-            </p>
-          </div>
+          <Row className="align-center">
+            <Col
+              xs="6"
+              className="justify-content-center	d-none d-lg-flex d-md-flex"
+            >
+              <StaticImage
+                src="./phone.png"
+                alt="Phone image"
+                objectFit="contain"
+                width={476}
+              />
+            </Col>
+            <Col md="6" xs="8">
+              <h1 className="mb-3">
+                38 laws to
+                <br />
+                avoid startup
+                <br />
+                disaster
+              </h1>
+              <div className="mb-3">Download your free pdf</div>
+              <div>Leave your details below:</div>
+              <OgilvyInlineForm
+                formId={formId}
+                buttonProps={{
+                  variant: "primary",
+                }}
+                recaptchaProps={{
+                  theme: "light",
+                  className: "mt-3",
+                }}
+              />
+            </Col>
+          </Row>
         </Container>
-        <div className="d-lg-flex">
-          <div className={styles.blueSection}>
-            <Container className="d-block d-lg-none">
-              <div>
-                <h1 className="mb-6 mt-4 mt-lg-0 mb-lg-5">
-                  38 laws to avoid startup disaster
-                </h1>
-                <p>
-                  As a growth agency, we have worked with some of the
-                  fastest-growing tech companies like TikTok, Alpian, Glowforge
-                  and more.
-                </p>
-                <p className="mb-5 mb-lg-0">
-                  We collected the 38 most important lessons that we’ve learned
-                  along the way.
-                </p>
-              </div>
-            </Container>
-            <div className={styles.lines}></div>
-            <div></div>
-          </div>
-          <div className={styles.greySection}>
-            <Row className="px-3 px-md-0">
-              <h2>Download your free copy</h2>
-              <Col md={6}>
-                <OgilvyForm
-                  formId={formId}
-                  buttonProps={{
-                    variant: "secondary",
-                  }}
-                  recaptchaProps={{
-                    theme: "light",
-                    className: "mt-5",
-                  }}
-                />
-              </Col>
-            </Row>
-          </div>
+        <div className={styles.mobileImage}>
+          <StaticImage
+            src="./phone_mobile.png"
+            alt="Phone image"
+            objectFit="contain"
+            width={120}
+            quality={100}
+          />
         </div>
       </div>
+      <OgilvyLearnForm
+        formId={formId}
+        buttonProps={{
+          variant: "primary",
+        }}
+        recaptchaProps={{
+          theme: "light",
+          className: "mt-3",
+        }}
+      />
     </>
   );
 };
 
-export default OglviyConcept2;
+export default OglviyConcept4;
