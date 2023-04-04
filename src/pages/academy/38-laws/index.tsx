551f69f6a9d4375076ee12aaf5a23e76cbfcda70
RekaSzendi <reka@double-agency.com>
2023-04-04 17:34:51 +0800

    academy new pages 0404

---
+++ src/pages/academy/38-laws/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  SEO,
+  OgilvyInlineForm,
+  OgilvyLearnForm,
+  AppHead,
+} from "@app/components";
+
+import * as styles from "./index.module.scss";
+import { StaticImage } from "gatsby-plugin-image";
+
+const formId = "5015398";
+
+const OglviyConcept4 = () => {
+  return (
+    <>
+      <div className={styles.pageHero}>
+        <Container>
+          <Row className="align-center">
+            <Col
+              xs="6"
+              className="justify-content-center	d-none d-lg-flex d-md-flex"
+            >
+              <StaticImage
+                src="./phone.png"
+                alt="Phone image"
+                objectFit="contain"
+                width={476}
+              />
+            </Col>
+            <Col md="6" xs="8">
+              <h1 className="mb-3">
+                38 laws to
+                <br />
+                avoid startup
+                <br />
+                disaster
+              </h1>
+              <div className="mb-3">Download your free pdf</div>
+              <div>Leave your details below:</div>
+              <OgilvyInlineForm
+                formId={formId}
+                buttonProps={{
+                  variant: "primary",
+                }}
+                recaptchaProps={{
+                  theme: "light",
+                  className: "mt-3",
+                }}
+              />
+            </Col>
+          </Row>
+        </Container>
+        <div className={styles.mobileImage}>
+          <StaticImage
+            src="./phone_mobile.png"
+            alt="Phone image"
+            objectFit="contain"
+            width={120}
+            quality={100}
+          />
+        </div>
+      </div>
+      <OgilvyLearnForm
+        formId={formId}
+        buttonProps={{
+          variant: "primary",
+        }}
+        recaptchaProps={{
+          theme: "light",
+          className: "mt-3",
+        }}
+      />
+    </>
+  );
+};
+
+export default OglviyConcept4;
+
+export const Head = () => {
+  return (
+    <>
+      <AppHead />
+      <SEO title="38 laws to avoid startup disaster" />
+    </>
+  );
+};
