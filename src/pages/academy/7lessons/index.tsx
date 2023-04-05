1374e0e4a910834fe391a056481b43354c8fbfd7
RekaSzendi <reka@double-agency.com>
2023-04-05 17:38:44 +0800

    academy 7 lessons page

--- src/pages/academy/7lessons/index.tsx
+++ src/pages/academy/7lessons/index.tsx
@@ -1,86 +1,88 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { SEO, OgilvyForm, AppHead } from "@app/components";
+import {
+  SEO,
+  OgilvyInlineForm,
+  OgilvyLearnForm,
+  AppHead,
+} from "@app/components";
 
 import * as styles from "./index.module.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
-const formId = "501415057";
+const formId = "5019059";
 
-const OglviyConcept2 = () => {
+const OglviyConcept4 = () => {
   return (
     <>
-      <div>
+      <div className={styles.pageHero}>
         <Container>
-          <div className={styles.heading}>
-            <h1>
-              7 strategic lessons <div>for CMOs </div>
-            </h1>
-            <p className="mt-5">
-              As a growth agency, we have worked with some of the
-              fastest-growing tech companies like TikTok, Alpian, Glowforge and
-              more.
-            </p>
-            <p className="mb-5 mb-lg-0">
-              We collected the 7 most important lessons that a CMO should follow
-              to succeed.
-            </p>
-          </div>
+          <Row className="align-center">
+            <Col
+              xs="6"
+              className="justify-content-center	d-none d-lg-flex d-md-flex"
+            >
+              <StaticImage
+                src="./phone.png"
+                alt="Phone image"
+                objectFit="contain"
+                width={476}
+              />
+            </Col>
+            <Col md="6" xs="8">
+              <h1 className="mb-3">
+                7 essential 
+                <br />
+                lessons on how
+                <br />
+                to grow startups
+              </h1>
+              <div className="mb-3">Get the free email course</div>
+              <div>Leave your details below:</div>
+              <OgilvyInlineForm
+                formId={formId}
+                buttonProps={{
+                  variant: "primary",
+                }}
+                recaptchaProps={{
+                  theme: "light",
+                  className: "mt-3",
+                }}
+              />
+            </Col>
+          </Row>
         </Container>
-        <div className="d-lg-flex">
-          <div className={styles.blueSection}>
-            <Container className="d-block d-lg-none">
-              <div>
-                <h1 className="mb-6 mt-4 mt-lg-0 mb-lg-5">
-                  7 strategic lessons for CMOs
-                </h1>
-                <p>
-                  As a growth agency, we have worked with some of the
-                  fastest-growing tech companies like TikTok, Alpian, Glowforge
-                  and more.
-                </p>
-                <p className="mb-5 mb-lg-0">
-                  We collected the 7 most important lessons that a CMO should
-                  follow to succeed.
-                </p>
-              </div>
-            </Container>
-            <div className={styles.lines}></div>
-            <div></div>
-          </div>
-          <div className={styles.greySection}>
-            <Row className="px-3 px-md-0">
-              <h2>Get the lessons for free</h2>
-              <Col md={6}>
-                <OgilvyForm
-                  formId={formId}
-                  buttonProps={{
-                    variant: "secondary",
-                  }}
-                  recaptchaProps={{
-                    theme: "light",
-                    className: "mt-5",
-                  }}
-                />
-              </Col>
-            </Row>
-          </div>
+        <div className={styles.mobileImage}>
+          <StaticImage
+            src="./phone_mobile.png"
+            alt="Phone image"
+            objectFit="contain"
+            width={120}
+            quality={100}
+          />
         </div>
       </div>
+      <OgilvyLearnForm
+        formId={formId}
+        buttonProps={{
+          variant: "primary",
+        }}
+        recaptchaProps={{
+          theme: "light",
+          className: "mt-3",
+        }}
+      />
     </>
   );
 };
 
-export default OglviyConcept2;
+export default OglviyConcept4;
 
 export const Head = () => {
   return (
     <>
       <AppHead />
-      <SEO
-        title="7 Strategic Lessons for CMOs from the Growth Agency"
-        keywords="strategic lessons for CMOs, Growth Agency"
-        description="Looking to up your marketing game? Check out these seven strategic lessons from some of the fastest-growing tech companies. Start learning from the best now."
-      />
+      <SEO title="38 laws to avoid startup disaster" />
     </>
   );
 };
