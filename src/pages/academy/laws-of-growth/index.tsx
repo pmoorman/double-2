2666fb52099247a5ce65afcdf83ac932d92eee39
RekaSzendi <reka@double-agency.com>
2023-04-14 11:59:06 +0200

    academy landing page image fix 0412

--- src/pages/academy/laws-of-growth/index.tsx
+++ src/pages/academy/laws-of-growth/index.tsx
@@ -1,83 +1,89 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { SEO, OgilvyForm, AppHead } from "@app/components";
+import {
+  SEO,
+  OgilvyInlineForm,
+  OgilvyLearnForm,
+  AppHead,
+} from "@app/components";
 
 import * as styles from "./index.module.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
 const formId = "4880150";
 
-const OglviyConcept2 = () => {
+const OglviyConcept4 = () => {
   return (
     <>
-      <div>
+      <div className={styles.pageHero}>
         <Container>
-          <div className={styles.heading}>
-            <h1>
-              Master the 38 <div>immutable </div>
-              <div> laws of growth</div>
-            </h1>
-            <p className="mt-5">
-              As a growth agency, we have worked with some of the
-              fastest-growing tech companies like TikTok, Alpian, Glowforge and
-              more.
-            </p>
-            <p className="mb-5 mb-lg-0">
-              We collected the 38 most important lessons that we’ve learned
-              along the way.
-            </p>
-          </div>
-        </Container>
-        <div className="d-lg-flex">
-          <div className={styles.blueSection}>
-            <Container className="d-block d-lg-none">
-              <div>
-                <h1 className="mb-6 mt-4 mt-lg-0 mb-lg-5">
-                  Master the 38 immutable laws of growth
-                </h1>
-                <p>
-                  As a growth agency, we have worked with some of the
-                  fastest-growing tech companies like TikTok, Alpian, Glowforge
-                  and more.
-                </p>
-                <p className="mb-5 mb-lg-0">
-                  We collected the 38 most important lessons that we’ve learned
-                  along the way.
-                </p>
+          <Row className="align-center">
+            <Col
+              xs="6"
+              className="justify-content-center	d-none d-lg-flex d-md-flex"
+            >
+              <StaticImage
+                src="./phone.png"
+                alt="Phone image"
+                objectFit="contain"
+                width={476}
+              />
+            </Col>
+            <Col md="6" xs="8">
+              <h1 className="mb-3">
+                The 38 laws of
+                <br />
+                growth marketing
+              </h1>
+              <div style={{ fontSize: "20px" }} className="mb-3">
+                As a growth agency, Double has executed campaigns for some of
+                the fastest growing tech companies in the world.
               </div>
-            </Container>
-            <div className={styles.lines}></div>
-            <div></div>
-          </div>
-          <div className={styles.greySection}>
-            <Row className="px-3 px-md-0">
-              <h2>Download your free copy</h2>
-              <Col md={6}>
-                <OgilvyForm
-                  formId={formId}
-                  buttonProps={{
-                    variant: "secondary",
-                  }}
-                  recaptchaProps={{
-                    theme: "light",
-                    className: "mt-5",
-                  }}
-                />
-              </Col>
-            </Row>
-          </div>
+              <div>Leave your details and get access to the free PDF.</div>
+              <OgilvyInlineForm
+                formId={formId}
+                buttonProps={{
+                  variant: "primary",
+                }}
+                recaptchaProps={{
+                  theme: "light",
+                  className: "mt-3",
+                }}
+              />
+            </Col>
+          </Row>
+        </Container>
+        <div className={styles.mobileImage}>
+          <StaticImage
+            src="./phone_mobile.png"
+            alt="Phone image"
+            objectFit="contain"
+            width={120}
+            quality={100}
+          />
         </div>
       </div>
+      <OgilvyLearnForm
+        formId={formId}
+        buttonProps={{
+          variant: "primary",
+        }}
+        recaptchaProps={{
+          theme: "light",
+          className: "mt-3",
+        }}
+      />
     </>
   );
 };
 
-export default OglviyConcept2;
+export default OglviyConcept4;
 
 export const Head = () => {
   return (
     <>
       <AppHead />
-      <SEO title="38 laws of growth" />
+      <SEO title="38 laws of growth marketing" />
     </>
   );
 };
