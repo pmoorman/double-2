0fd30f5a0f913a4c208201348fabeb193d7f245a
A <anthonyc.animba@gmail.com>
2023-10-22 20:35:30 +0700

    /

--- src/components/about-section/index.tsx
+++ src/components/about-section/index.tsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
         <Row>
           <Col>
             <h2>About our clients</h2>
-            <h4>No matter which stage you’re at, we work with everyone.</h4>
+            <h4>no matter which stage you’re at, we work with everyone.</h4>
           </Col>
         </Row>
       </Container>
