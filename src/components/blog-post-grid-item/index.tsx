926b09153edd5ce1c795b99544c8389c89dc9f68
anthonyc-a <anthonyc.animba@gmail.com>
2023-10-28 12:55:36 +0700

    .

--- src/components/blog-post-grid-item/index.tsx
+++ src/components/blog-post-grid-item/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
 import cn from "classnames";
@@ -8,6 +8,7 @@ import { Link } from "@app/components";
 
 import * as styles from "./index.module.scss";
 import { format } from "date-fns";
+import gsap from "gsap";
 
 export interface BlogPostGridItemProps {
   post: BlogPost;
@@ -17,8 +18,29 @@ export const BlogPostGridItem: FC<BlogPostGridItemProps> = ({ post }) => {
   const date = format(new Date(post.date), "dd MMM yyyy");
   const categories = post.categories.join(", ");
 
+  const itemRef = useRef(null);
+
+  useEffect(() => {
+    const el = itemRef.current;
+
+    gsap.from(el, {
+      y: 8,
+      opacity: 0,
+      duration: .35,
+      delay: Math.random() * .75, // random delay
+      ease: "power3",
+
+      scrollTrigger: {
+        trigger: el,
+        start: "top 100%",
+        toggleActions: "play none none reverse",
+        once: true, // only animate once
+      },
+    });
+  }, []);
+
   return (
-    <Col md="6" lg="4" sm="12" className={cn(`my-lg-4 my-2`, styles.card)}>
+    <Col ref={itemRef} md="6" lg="4" sm="12" className={cn(`my-lg-4 my-2`, styles.card)}>
       <Link to={post.slug}>
         <GatsbyImage
           className={cn("mb-3", styles.image)}
